7b0a8e39e476255fdbf339ac23d9a8f34c7397df
Anikey <eagleremlinh@gmail.com>
2023-12-08 20:37:20 +0700

    G

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -17,34 +17,13 @@ const Page = () => {
         <div className='About'>
             {check ?
                 <>
-                    <div className="rocket">
-                        <div className="rocket-body">
-                        <div className="body"></div>
-                        <div className="fin fin-left"></div>
-                        <div className="fin fin-right"></div>
-                        <div className="window"></div>
-                        </div>
-                        <div className="exhaust-flame"></div>
-                        <ul className="exhaust-fumes">
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        </ul>
-                        <ul className="star">
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        </ul>
+                    <div className="spinner">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
                     </div>
                     <h3 className="title-loading" data-aos="fade-up">Starting up, please wait a moment!...</h3>
                 </>
