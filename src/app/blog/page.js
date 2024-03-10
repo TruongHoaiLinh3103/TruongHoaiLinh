ea7b74c4ea9d5f64d008ac051bbb9f566cfdd8f9
Anikey <eagleremlinh@gmail.com>
2024-03-10 22:14:20 +0700

    End

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -7,49 +7,14 @@ import FetchAOS from "../../utils/FetchAOS";
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Typewriter } from 'react-simple-typewriter';
-import { WEBSITE } from '../../story/WEBSITE';
-import { GAME } from '../../story/GAME';
 import { DETAIL } from "../../story/DETAIL"
-import { FaKey  } from "react-icons/fa";
 import { FaBackward, FaAddressCard, FaBible , FaFileContract } from "react-icons/fa";
-import DOMPurify from 'dompurify';
 
 const Page = () => {
     const {check} = FetchLoading();
-    const [modal, setModal] = useState(false);
     FetchAOS();
     const [number, setNumber] = useState(1);
     const router = useRouter();
-    const [pass, setPass] = useState("");
-    const [open, setOpen] = useState(false);
-    const [id, setID] = useState(0)
-    const numberOne = () => {
-        setNumber(1)
-    }
-    const numberTwo = () => {
-        setNumber(2)
-    }
-    const numberThree = () => {
-        setNumber(3)
-    }
-    const numberFour = () => {
-        setNumber(4)
-    }
-    const numberFive = () => {
-        setNumber(5)
-    }
-    const openModal = (item) => {
-        setID(item.id)
-        setModal(true);
-    }
-    const closeModal = () => {
-        setID(0)
-        setModal(false);
-    }
-    const detailContent = (contents) => {
-        const htmlSanatized = DOMPurify.sanitize(contents);
-        return htmlSanatized;
-    }
     return (
         <>
             {check ? (
@@ -79,7 +44,7 @@ const Page = () => {
             <div className='Box'>
                 <div>
                     <div className="conteudo__geral">
-                        <div className="elemento__fotos elemento__1" onClick={() => numberOne()}>
+                        <div className="elemento__fotos elemento__1" onClick={() => setNumber(1)}>
                             <Typewriter
                                 cursor
                                 loop
@@ -92,15 +57,15 @@ const Page = () => {
                             />
                         </div>
                         
-                        <div className="elemento__fotos elemento__2" onClick={() => numberTwo()}></div>
+                        <div className="elemento__fotos elemento__2" onClick={() => setNumber(2)}></div>
 
-                        <div className="elemento__fotos elemento__3" onClick={() => numberThree()}>
+                        <div className="elemento__fotos elemento__3">
                             <div className="titulo__inicial">Hover Me!</div>
                         </div>
 
-                        <div className="elemento__fotos elemento__4" onClick={() => numberFour()}></div>
+                        <div className="elemento__fotos elemento__4" onClick={() => setNumber(3)}></div>
 
-                        <div className="elemento__fotos elemento__5" onClick={() => numberFive()}>
+                        <div className="elemento__fotos elemento__5" onClick={() => setNumber(4)}>
                             <Typewriter
                                 cursor
                                 loop
@@ -109,7 +74,7 @@ const Page = () => {
                                 typeSpeed={70}
                                 deleteSpeed={50}
                                 delaySpeed={1000}
-                                words={["Website"]}
+                                words={["Sport"]}
                             />
                         </div>
                     </div>
@@ -117,156 +82,59 @@ const Page = () => {
                 <div className='Box-message'>
                     {number === 1 &&
                         <div className='Box-message-item' data-aos="fade-up">
-                            {GAME.map((item) => {
+                            {DETAIL.map((item) => {
                                 return(
-                                    <div key={item.id} className='Box-item_img-cover'>
-                                        <img src={item.img} alt={item.name} onClick={() => openModal(item)}/>
+                                    <div key={item.id}>
+                                        { item.id < 6 && 
+                                            <div className='Box-item_img-cover' onClick={() => router.push(item.img)}>
+                                                <img src={item.img} alt={item.name}/>
+                                            </div>
+                                        }
                                     </div>
                                 )
                             })}
                         </div>
                     }
                     {number === 2 &&
-                        <>
-                            <div className='Box-message-item'>
-                                {DETAIL.map((item) => {
-                                    return(
-                                        <div key={item.id}>
-                                            { item.id > 8 && item.id < 13 && 
-                                                <div className='Box-item_img-cover'>
-                                                    <img src={item.img} alt={item.name} />
-                                                </div>
-                                            }
-                                        </div>
-                                    )
-                                })}
-                            </div>
-                            <div className='Box-message-item'>
-                                {DETAIL.map((item) => {
-                                    return(
-                                        <div key={item.id}>
-                                            {item.id > 12 &&
-                                                <div className='Box-item_img-cover'>
-                                                    <img src={item.img} alt={item.name} />
-                                                </div>
-                                            }
-                                        </div>
-                                    )
-                                })}
-                            </div>
-                        </>
+                        <div className='Box-message-item' data-aos="fade-up">
+                            {DETAIL.map((item) => {
+                                return(
+                                    <div key={item.id}>
+                                        {item.id > 5 && item.id < 11 &&
+                                            <div className='Box-item_img-cover' onClick={() => router.push(item.img)}>
+                                                <img src={item.img} alt={item.name}/>
+                                            </div>
+                                        }
+                                    </div>
+                                )
+                            })}
+                        </div>
                     }
                     {number === 3 &&
-                        <>
-                            <div className='Box-message-input' style={{display: !open ? "flex" : "none"}} data-aos="fade-up">
-                                <div style={{display: "flex", alignItems: "center", flexDirection: "column"}}>
-                                    <input type='password' onChange={(e) => {setPass(e.target.value)}} 
-                                    value={pass} placeholder='Password (ADMIN)' 
-                                    style={{padding: "10px", border: "3px solid black", fontSize: "15px"}}/>
-                                    <button 
-                                    style={{padding: "10px", backgroundColor:"black", color: "white", border: "3px solid white", fontSize: "15px", marginTop: "20px"}} 
-                                    onClick={() => {pass==="Ahkoeptr3103?" ? setOpen(true): alert("Muốn biết à :)), Đừng nghĩ nhiểu!")}}>
-                                    <FaKey /></button>
-                                </div>
-                            </div>
-                            {open && 
-                                <div className='Box-message-item'>
-                                    <div className='Box-item_content-cover'>
-                                        <h3>Diện</h3>
-                                        <select>
-                                            <option>Diện cầu</option>
-                                            <option>Diện lộ</option>
-                                            <option>Diện thượng đơn</option>
-                                            <option>Diện hạ nguyên</option>
-                                            <option>Diện chia thượng</option>
-                                            <option>Diện loạn khôi</option>
-                                            <option>Diện thấu</option>
-                                        </select>
+                        <div className='Box-message-item' data-aos="fade-up">
+                            {DETAIL.map((item) => {
+                                return(
+                                    <div key={item.id}>
+                                        {item.id > 10 && item.id < 16 && 
+                                            <div className='Box-item_img-cover' onClick={() => router.push(item.img)}>
+                                                <img src={item.img} alt={item.name}/>
+                                            </div>
+                                        }
                                     </div>
-                                    <div className='Box-item_content-cover'>
-                                        <h3>Ẩn</h3>
-                                        <select>
-                                            <option>Ẩn son</option>
-                                            <option>Ẩn tráo</option>
-                                            <option>Ẩn như</option>
-                                            <option>Ẩn chia hạ</option>
-                                            <option>Ẩn đôi</option>
-                                            <option>Ẩn loạn thượng đơn</option>
-                                            <option>Ẩn loạn thượng hạ song</option>
-                                            <option>Ẩn như phật tiền</option>
-                                            <option>Ẩn đả địa</option>
-                                        </select>
-                                    </div>
-                                    <div className='Box-item_content-cover'>
-                                        <h3>Ảo</h3>
-                                        <select>
-                                            <option>Ảo danh nhị</option>
-                                            <option>Ảo danh nhất</option>
-                                            <option>Ảo phi khống</option>
-                                            <option>Ảo lưỡng nghi</option>
-                                            <option>Ảo tam chỉ</option>
-                                            <option>Ảo trảm danh ngũ</option>
-                                            <option>Ảo trảm danh nhị</option>
-                                            <option>Ảo trảm thượng đơn hạ hạ</option>
-                                            <option>Ảo trảm giả ẩn son</option>
-                                            <option>Ảo kiếp phù du</option>
-                                        </select>
-                                    </div>
-                                    <div className='Box-item_content-cover'>
-                                        <h3>Thao</h3>
-                                        <select>
-                                            <option>Thao nhập song tứ</option>
-                                            <option>Thao nhập thượng ngũ tam</option>
-                                            <option>Thao nhập thượng hạ tứ tam</option>
-                                            <option>Thao nhập tam nhị tam</option>
-                                            <option>Thao nhập hạ song tam</option>
-                                            <option>Thao thâu thượng</option>
-                                            <option>Thao thâu hạ</option>
-                                            <option>Thao thâu thượng đơn</option>
-                                            <option>Thao thâu số trong</option>
-                                            <option>Thao thâu chuẩn</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            }
-                        </>
+                                )
+                            })}
+                        </div>
                     }
                     {number === 4 &&
-                        <>
-                            <div className='Box-message-item'>
-                                {DETAIL.map((item) => {
-                                    return(
-                                        <div key={item.id}>
-                                            {item.id < 5 && 
-                                                <div className='Box-item_img-cover'>
-                                                    <img src={item.img} alt={item.name} />
-                                                </div>
-                                            }
-                                        </div>
-                                    )
-                                })}
-                            </div>
-                            <div className='Box-message-item'>
-                                {DETAIL.map((item) => {
-                                    return(
-                                        <div key={item.id}>
-                                            {item.id > 4 && item.id < 9 &&
-                                                <div className='Box-item_img-cover'>
-                                                    <img src={item.img} alt={item.name} />
-                                                </div>
-                                            }
-                                        </div>
-                                    )
-                                })}
-                            </div>
-                        </>
-                    }
-                    {number === 5 &&
                         <div className='Box-message-item' data-aos="fade-up">
-                            {WEBSITE.map((item) => {
+                            {DETAIL.map((item) => {
                                 return(
-                                    <div key={item.id} className='Box-item_img-cover'>
-                                        <img title={item.title} src={item.img} alt="website"/>
+                                    <div key={item.id}>
+                                        {item.id > 15 &&
+                                            <div className='Box-item_img-cover' onClick={() => router.push(item.img)}>
+                                                <img src={item.img} alt={item.name}/>
+                                            </div>
+                                        }
                                     </div>
                                 )
                             })}
@@ -279,28 +147,6 @@ const Page = () => {
                     <li title='Project' onClick={() => {router.push("/project")}}><FaBible /></li>
                     <li title='Contact' onClick={() => {router.push("/contact")}}><FaFileContract /></li>
                 </ul>
-                <div className='modal' style={{display: modal ? "flex" : "none"}}>
-                    {number === 1 &&
-                    <div className='box'>
-                        {GAME.map((item) => {
-                            return(
-                                id === item.id &&
-                                <form key={item.id}>
-                                    <div>
-                                        <img src={item.img} alt={item.name} />
-                                    </div>
-                                    <div>
-                                        <input type='text' defaultValue={item.name} disabled/>
-                                    </div>
-                                    <div dangerouslySetInnerHTML={{__html: detailContent(item.text)}}></div>
-                                </form>
-                            )
-                        })}
-                        <div className='btn-form'>
-                            <button onClick={() => closeModal()}>Close</button>
-                        </div>
-                    </div>}
-                </div>
             </div>
             )
             }
--- src/story/DETAIL.js
+++ src/story/DETAIL.js
@@ -2,36 +2,36 @@ export const DETAIL = [
     {
       "id": 1,
       "img": "https://i.pinimg.com/originals/9b/a4/65/9ba465f1d0f1e7581f3aafe8b78fac19.png",
-      "name": "other"
+      "name": "game"
     },
     {
       "id": 2,
       "img": "https://i.pinimg.com/originals/c3/ed/b8/c3edb81da306b1f26a46bb2def79ea49.jpg",
-      "name": "other"
+      "name": "game"
     },
     {
       "id": 3,
       "img": "https://i.pinimg.com/originals/3e/1a/c9/3e1ac97d5b7e4196a5dbc9e93e760ae9.jpg",
-      "name": "other"
+      "name": "game"
     },
     {
       "id": 4,
-      "img": "https://i.pinimg.com/originals/be/ed/23/beed23ec1415c8b7aa2abdbd5467d163.jpg",
-      "name": "other"
+      "img": "https://i.pinimg.com/originals/e9/7c/4a/e97c4a36fc238d71037411bb56317020.jpg",
+      "name": "game"
     },
     {
       "id": 5,
+      "img": "https://i.pinimg.com/originals/ee/10/d8/ee10d897a67a31be7bbdfed10b30c1da.png",
+      "name": "game"
+    },
+    {
+      "id": 6,
       "img": "https://i.pinimg.com/originals/28/86/85/288685640b9c1f4dd71a1dc96d0f7365.jpg",
       "name": "comic"
     },
     {
-      "id": 6,
-      "img": "https://i.pinimg.com/originals/fb/02/ef/fb02efc76af03f42323065674e1bc4ba.jpg",
-      "name": "comic"
-    },
-    {
       "id": 7,
-      "img": "https://i.pinimg.com/originals/9b/87/27/9b8727d573f5f6b18e3583ec526c0eca.jpg",
+      "img": "https://i.pinimg.com/originals/fb/02/ef/fb02efc76af03f42323065674e1bc4ba.jpg",
       "name": "comic"
     },
     {
@@ -41,27 +41,27 @@ export const DETAIL = [
     },
     {
       "id": 9,
-      "img": "https://i.pinimg.com/originals/d2/57/b9/d257b99075cc860b23048112ebae97c0.jpg",
-      "name": "sport"
+      "img": "https://i.pinimg.com/originals/9b/87/27/9b8727d573f5f6b18e3583ec526c0eca.jpg",
+      "name": "comic"
     },
     {
       "id": 10,
-      "img": "https://i.pinimg.com/originals/66/36/4f/66364f9824b421c3f3b2d1f23ea743f6.png",
-      "name": "sport"
+      "img": "https://i.pinimg.com/originals/28/a3/4c/28a34c675b7e14acbcc584db633e9cca.jpg",
+      "name": "comic"
     },
     {
       "id": 11,
-      "img": "https://i.pinimg.com/originals/83/14/81/831481b325e814d02d48da216b2f3d17.png",
-      "name": "sport"
+      "img": "https://i.pinimg.com/originals/94/10/ba/9410bab0186b52905d639979e13c0ade.jpg",
+      "name": "food"
     },
     {
       "id": 12,
-      "img": "https://i.pinimg.com/originals/43/cc/c8/43ccc879ac920577ed010229d5bf97ef.jpg",
-      "name": "sport"
+      "img": "https://i.pinimg.com/originals/4d/a8/e2/4da8e27f6a9bc917c23ebed5dfad758e.jpg",
+      "name": "food"
     },
     {
       "id": 13,
-      "img": "https://i.pinimg.com/originals/94/10/ba/9410bab0186b52905d639979e13c0ade.jpg",
+      "img": "https://i.pinimg.com/originals/d6/df/f0/d6dff03bc5e48070d5280fc1a979ab06.jpg",
       "name": "food"
     },
     {
@@ -71,12 +71,32 @@ export const DETAIL = [
     },
     {
       "id": 15,
-      "img": "https://i.pinimg.com/originals/4d/a8/e2/4da8e27f6a9bc917c23ebed5dfad758e.jpg",
+      "img": "https://i.pinimg.com/originals/e3/3c/28/e33c28567e9f064eac2e205d5a97a5e3.jpg",
       "name": "food"
     },
     {
       "id": 16,
-      "img": "https://i.pinimg.com/originals/e3/3c/28/e33c28567e9f064eac2e205d5a97a5e3.jpg",
-      "name": "food"
+      "img": "https://i.pinimg.com/originals/d2/57/b9/d257b99075cc860b23048112ebae97c0.jpg",
+      "name": "sport"
+    },
+    {
+      "id": 17,
+      "img": "https://i.pinimg.com/originals/66/36/4f/66364f9824b421c3f3b2d1f23ea743f6.png",
+      "name": "sport"
+    },
+    {
+      "id": 18,
+      "img": "https://i.pinimg.com/originals/83/14/81/831481b325e814d02d48da216b2f3d17.png",
+      "name": "sport"
+    },
+    {
+      "id": 19,
+      "img": "https://i.pinimg.com/originals/43/cc/c8/43ccc879ac920577ed010229d5bf97ef.jpg",
+      "name": "sport"
+    },
+    {
+      "id": 20,
+      "img": "https://i.pinimg.com/originals/be/ed/23/beed23ec1415c8b7aa2abdbd5467d163.jpg",
+      "name": "sport"
     }
 ]
