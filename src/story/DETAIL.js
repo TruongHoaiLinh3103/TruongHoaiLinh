0a3e2dd3a2a5adb7e3ce9a05e1470f578e636d10
Trương Hoài Linh <131947376+TruongHoaiLinh3103@users.noreply.github.com>
2024-03-22 15:14:19 +0700

    Update DETAIL.js

--- src/story/DETAIL.js
+++ src/story/DETAIL.js
@@ -1,102 +1,82 @@
 export const DETAIL = [
     {
       "id": 1,
-      "img": "https://i.pinimg.com/originals/9b/a4/65/9ba465f1d0f1e7581f3aafe8b78fac19.png",
-      "name": "game"
-    },
-    {
-      "id": 2,
       "img": "https://i.pinimg.com/originals/c3/ed/b8/c3edb81da306b1f26a46bb2def79ea49.jpg",
       "name": "game"
     },
     {
-      "id": 3,
+      "id": 2,
       "img": "https://i.pinimg.com/originals/3e/1a/c9/3e1ac97d5b7e4196a5dbc9e93e760ae9.jpg",
       "name": "game"
     },
     {
-      "id": 4,
+      "id": 3,
       "img": "https://i.pinimg.com/originals/e9/7c/4a/e97c4a36fc238d71037411bb56317020.jpg",
       "name": "game"
     },
     {
-      "id": 5,
+      "id": 4,
       "img": "https://i.pinimg.com/originals/ba/94/4b/ba944bea6c57c42d6fbc6dbe04d1fddd.png",
       "name": "game"
     },
     {
-      "id": 6,
+      "id": 5,
       "img": "https://i.pinimg.com/originals/28/86/85/288685640b9c1f4dd71a1dc96d0f7365.jpg",
       "name": "comic"
     },
     {
-      "id": 7,
+      "id": 6,
       "img": "https://i.pinimg.com/originals/fb/02/ef/fb02efc76af03f42323065674e1bc4ba.jpg",
       "name": "comic"
     },
     {
-      "id": 8,
-      "img": "https://i.pinimg.com/originals/16/73/4b/16734bbdde488229730959ea4eb204fd.jpg",
+      "id": 7,
+      "img": "https://i.pinimg.com/originals/4f/eb/9f/4feb9f5f60c5d0b87051473951456e37.jpg",
       "name": "comic"
     },
     {
-      "id": 9,
+      "id": 8,
       "img": "https://i.pinimg.com/originals/9b/87/27/9b8727d573f5f6b18e3583ec526c0eca.jpg",
       "name": "comic"
     },
     {
-      "id": 10,
-      "img": "https://i.pinimg.com/originals/5a/e1/58/5ae15822d73ac828599044153fb91fed.jpg",
-      "name": "comic"
-    },
-    {
-      "id": 11,
+      "id": 9,
       "img": "https://i.pinimg.com/originals/94/10/ba/9410bab0186b52905d639979e13c0ade.jpg",
       "name": "food"
     },
     {
-      "id": 12,
+      "id": 10,
       "img": "https://i.pinimg.com/originals/4d/a8/e2/4da8e27f6a9bc917c23ebed5dfad758e.jpg",
       "name": "food"
     },
     {
-      "id": 13,
-      "img": "https://i.pinimg.com/originals/b7/93/10/b79310f710bea5271f89e06bac022832.jpg",
-      "name": "food"
-    },
-    {
-      "id": 14,
+      "id": 11,
       "img": "https://i.pinimg.com/originals/84/89/bc/8489bc53d1c34959fa1c04f41a960295.jpg",
       "name": "food"
     },
     {
-      "id": 15,
+      "id": 12,
       "img": "https://i.pinimg.com/originals/e3/3c/28/e33c28567e9f064eac2e205d5a97a5e3.jpg",
       "name": "food"
     },
     {
-      "id": 16,
-      "img": "https://i.pinimg.com/originals/d2/57/b9/d257b99075cc860b23048112ebae97c0.jpg",
-      "name": "sport"
-    },
-    {
-      "id": 17,
+      "id": 13,
       "img": "https://i.pinimg.com/originals/66/36/4f/66364f9824b421c3f3b2d1f23ea743f6.png",
       "name": "sport"
     },
     {
-      "id": 18,
+      "id": 14,
       "img": "https://i.pinimg.com/originals/83/14/81/831481b325e814d02d48da216b2f3d17.png",
       "name": "sport"
     },
     {
-      "id": 19,
+      "id": 15,
       "img": "https://i.pinimg.com/originals/43/cc/c8/43ccc879ac920577ed010229d5bf97ef.jpg",
       "name": "sport"
     },
     {
-      "id": 20,
-      "img": "https://i.pinimg.com/originals/be/ed/23/beed23ec1415c8b7aa2abdbd5467d163.jpg",
+      "id": 16,
+      "img": "https://i.pinimg.com/originals/fd/c2/bf/fdc2bf47cebe665486197d6aefd27a17.jpg",
       "name": "sport"
     }
 ]
